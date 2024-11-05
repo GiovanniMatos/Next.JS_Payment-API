1ce796ea68e5e9e6f4638e9da67852cbc271a860
GiovanniMatos <giovannyafonso23@gmail.com>
2024-11-05 11:01:11 -0400

    pix payment

--- nextjs-checkout/src/components/Checkout/CheckoutPix.jsx
+++ nextjs-checkout/src/components/Checkout/CheckoutPix.jsx
@@ -3,47 +3,63 @@
 import { useState } from "react";
 
 export default function CheckoutPix(){
-    const [copied, setCopied] = useState(false);
 
-    const handleCopy = (text) => {
-      if (!navigator.clipboard) {
-        // Handle fallback for older browsers
-        return;
-      }
-      navigator.clipboard.writeText(text)
-        .then(() => {
-          console.log('Text copied to clipboard!');
-        })
-        .catch(err => {
-          console.error('Failed to copy text:',   
-        err);
-        });
-    };
-  
-    const handleClick = () => {
-      const textToCopy = PixCode; 
-      handleCopy(textToCopy);
-      setCopied(true); 
-    };
+  const [transaction_amount, setTransactionAmount] = useState();
+  const [description, setDescription] = useState();
+  const [email, setEmail] = useState();
+  const [cpf, setCPF] = useState();
 
-    const PixCode = 'your-pix-code-here';
-    return(
-        <>
-        <h1 className="mb-1 text-left ml-[-1.5rem] lg:ml-[0.20rem]">Credit Card Payment</h1>
-        <div className="lg:flex">
-            <div className="bg-gray-800 h-[14rem] w-[14rem] lg:h-[16.5rem] lg:w-[16.5rem] mx-auto lg:ml-0 lg:mr-1"/>
-            <div className="">
-                <input
-                className="w-full md:w-[30rem] lg:w-[30rem] lg:ml-0 pt-2 pb-2 pl-4 border-zinc-300 border-[0.5px] rounded-[4px] mt-3 text-gray-800 "
-                type="text"
-                defaultValue={PixCode}
-                readOnly
-                />
-                <button onClick={handleClick} className="font-bold text-white bg-green-500 pl-4 pr-4 pt-1 pb-1 rounded-md mt-1 flex mx-auto lg:ml-0">
-                    {copied ? 'Copiado!' : 'Copiar código'}
-                </button>
-            </div>
-        </div>
-        </>
-    )
+  async function payWithPix(event){
+    event.preventDefault(); 
+    try {
+        const response = await fetch("http://localhost:4000/pix-payment", {
+            method: "POST",
+            headers: {
+                mode: "no-cors",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                transaction_amount: Number(transaction_amount),
+                description: description,
+                email: email,
+                cpf: cpf
+            })
+          })
+
+    } catch (error) {
+        console.error("Error during payment:", error);
+    }
+}
+  return(
+      <>
+      <h1 className="mb-1 text-left ml-[-1.5rem] lg:ml-[0.20rem]">Pix Payment</h1>
+      <form onSubmit={payWithPix}>
+          <div className="lg:mb-4 mb-1">
+              <input type="number" id="transaction_amount"
+                className="shadow appearance-none border rounded lg:w-full ml-[-1.5rem] w-[23.3rem] lg:ml-0 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                placeholder="Transaction Amount"
+                onChange={(e) => setTransactionAmount(Number(e.target.value))}/>
+          </div>
+          <div className="lg:mb-4 mb-1">
+              <input type="text" id="description"
+                className="shadow appearance-none border rounded lg:w-full ml-[-1.5rem] w-[23.3rem] lg:ml-0 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                placeholder="Description (Not required)"
+                onChange={(e) => { setDescription(e.target.value)}}/>
+          </div>
+          <div className="lg:mb-4 mb-1">
+              <input type="email" id="email"
+                className="shadow appearance-none border rounded lg:w-full ml-[-1.5rem] w-[23.3rem] lg:ml-0 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                placeholder="E-mail"
+                onChange={(e) => { setEmail(e.target.value)}}/>
+          </div>
+          <div className="lg:mb-4 mb-1">
+              <input type="number" id="cpf"
+                className="shadow appearance-none border rounded lg:w-full ml-[-1.5rem] w-[23.3rem] lg:ml-0 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                placeholder="CPF"
+                onChange={(e) => { setCPF(e.target.value)}}/>
+          </div>
+          <button type="submit" className="ml-[-1.5rem] lg:ml-0 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Pay Now</button>
+      </form>
+      </>
+  )
 }
--- server/pixpayment.js
+++ server/pixpayment.js
@@ -1,3 +1,42 @@
+const express = require('express')
+const app = express()
+app.use(express.json());
 
+var cors = require('cors')
+app.use(cors());
 require('dotenv').config()
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+const { MercadoPagoConfig, Payment } = require('mercadopago'); // correct import
+const client = new MercadoPagoConfig({ 
+    accessToken: ACCESS_TOKEN, 
+    options: { timeout: 5000, idempotencyKey: 'abc' } });
+const payment = new Payment(client); //payment with your account
+
+app.post('/pix-payment', async (req, res) => {
+
+    const body =  { 
+        transaction_amount: req.body.transaction_amount,
+        description: req.body.description,
+        payment_method_id: "pix",
+            payer: {
+            email: req.body.email,
+            identification: {
+        type: "cpf",
+        number: req.body.number
+    }}}
+
+    try {
+        const paymentResponse = await payment.create({ body });
+        const ticketUrl = paymentResponse.point_of_interaction.transaction_data.ticket_url;
+        res.redirect(ticketUrl)
+    } catch (error) {
+        console.error('Error during Pix payment:', error);
+        res.status(500).send('Error during Pix payment');
+    }
+        
+})
+
+
+app.listen(4000, () => {
+    console.log('Server is running on http://localhost:4000');
+});
